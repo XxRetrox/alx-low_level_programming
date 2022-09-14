5577140f1460be7e3bd1f1c2e0950e0929dc9c20
Akhaze Jesse <jasonreacher47@gmail.com>
2022-09-14 13:58:29 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,15 @@
 
 
 void print_alphabet(void);
-
-int main(void)
 {
 
-	print_alphabet();
+	char ch;
 
+	for (ch = 'a'; ch <= 'z'; ch++)
+		{
+		_putchar(ch);
+		}
+	_putchar('\n');
 
-	return (0);
+	return;
 }
