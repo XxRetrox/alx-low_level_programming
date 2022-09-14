0861f403071bc927c5cea04d25595b8daf46b91f
Akhaze Jesse <jasonreacher47@gmail.com>
2022-09-14 15:07:36 -0700

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return:
+ */
+
+void print_alphabet_x10(void)
+{
+	char a1;
+	char ch;
+
+	for (a1 = 'o'; a1 <= '9'; a1++)
+		{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			{
+			putchar(ch);
+			}
+		putchar('\n');
+		}
+	return;
+
+}
