4218de5234df54bbc015b0fe9c83a8bcdb92f870
Akhaze Jesse <jasonreacher47@gmail.com>
2022-09-14 13:47:40 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 #include "main.h"
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+void print_alphabet(void)
+{
+	char ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	
+	{
+		_putchar(ch);
+	}
+
+	_putchar('\n');
+
+	return;
+}
 
 int main(void)
 {
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,21 +1,2 @@
-#include <unistd.h>
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-
-void print_alphabet(void)
-{
-	char ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-		{
-		_putchar(ch);
-		}
-
-	_putchar('\n');
-
-	return;
-}
+int _putchar(char c);
+void print_alphabet(void);
