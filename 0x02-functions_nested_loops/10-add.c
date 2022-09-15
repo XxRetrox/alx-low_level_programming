217eee72fe7211d80eacf362c5f7523ce318a460
Akhaze Jesse <jasonreacher47@gmail.com>
2022-09-14 18:42:56 -0700

    a

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @x: letter to be tested
+ * @y: letter to be tested
+ * Return: Always 0
+ */
+
+int add(int x, int y)
+{
+	add = x + y;
+
+	return (add);
+}
