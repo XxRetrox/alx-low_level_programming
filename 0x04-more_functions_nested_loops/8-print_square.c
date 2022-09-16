9051de26848ce4416f264532096b2893c2ce3b3e
Akhaze Jesse <jasonreacher47@gmail.com>
2022-09-16 15:44:31 -0700

    a

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_square - Entry
+ * @size: square
+ * Return: Always 0
+ */
+
+void print_square(int size)
+{
+	int u;
+	int a;
+	int b;
+
+	u = 35;
+
+	if (n >= 1)
+		{
+		for (a = 1; a <= n; a++)
+			{
+			for (b = 1; b <= n; b++)
+				{
+				_putchar (u);
+				}
+			_putchar ('\n');
+			}
+		}
+	else
+		{
+		_putchar ('\n');
+		}
+}
