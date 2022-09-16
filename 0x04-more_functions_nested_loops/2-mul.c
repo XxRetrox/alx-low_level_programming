b5854335ae7e4ee5761431ca2a914821b7cf223f
Akhaze Jesse <jasonreacher47@gmail.com>
2022-09-15 18:19:25 -0700

    a

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: first int
+ * @b: second int
+ * Return: Product
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}
