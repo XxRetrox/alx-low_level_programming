7655a55d42d1adc7c008076384ae7465de96bedb
Akhaze Jesse <jasonreacher47@gmail.com>
2022-09-20 00:38:28 -0700

    a

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - Entry
+ * @m: int
+ * @n: int
+ * Return: 0
+ */ 
+
+void swap_int(int *m, int *n)
+{
+	*m = *n;
+
+	*n = *m;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,2 +1,3 @@
 void reset_to_98(int *p);
 int _putchar(char c);
+void swap_int(int *m, int *n);
