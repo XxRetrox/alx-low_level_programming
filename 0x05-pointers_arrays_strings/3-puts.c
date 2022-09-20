7ff71420df8eaf2c52dcc506486fd985a06e5878
Akhaze Jesse <jasonreacher47@gmail.com>
2022-09-20 03:20:24 -0700

    a

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - Entry
+ * @str: string
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	*str;
+
+	_putchar ('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,3 +2,4 @@ void reset_to_98(int *p);
 int _putchar(char c);
 void swap_int(int *m, int *n);
 int _strlen(char *s);
+void _puts(char *str);
