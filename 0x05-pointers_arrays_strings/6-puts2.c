4925fb6d20bc20ccc5d431dadc5ba40dfc2880d9
Akhaze Jesse <jasonreacher47@gmail.com>
2022-09-20 08:24:52 -0700

    a

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry
+ * @str: string
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	for (a = 0; str[a] != '\0'; a + 2)
+		{
+		_putchar (str[a]);
+		}
+
+	_putchar ('\n');
+}
