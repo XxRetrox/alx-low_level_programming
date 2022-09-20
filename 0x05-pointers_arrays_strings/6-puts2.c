732c1ed09b890149c6e6c0227c6fed989194977b
Akhaze Jesse <jasonreacher47@gmail.com>
2022-09-20 08:40:13 -0700

    a

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,8 +10,12 @@ void puts2(char *str)
 {
 	int a;
 
-	for (a = 0; str[a] != '\0'; a + 2)
+	for (a = 0; str[a] != '\0'; a++)
 		{
+		if (a % 2 != 0)
+			{
+			continue;
+			}
 		_putchar (str[a]);
 		}
 
