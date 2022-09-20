fae39262190889b79399d6f0a3f3ba3085eaa5ca
Akhaze Jesse <jasonreacher47@gmail.com>
2022-09-20 09:08:44 -0700

    a

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - Entry
+ * @str: string
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int a;
+	int b;
+	int c;
+	int len;
+
+	len = strlen(str);
+	b = len / 2;
+	c = b + 1;
+
+	if (len % 2 == 0)
+		{
+		for (a = b; str[a] != '\0'; a++)
+			{
+			_putchar (str[a]);
+			}
+		}
+	else if (len % 2 != 0)
+		{
+		for (a = c; str[a] != '\n'; a++)
+			{
+			_putchar (str[a]);
+			}
+		}
+	else
+		{
+		_putchar (len);
+		}
+
+	_putchar ('\n');
+}
