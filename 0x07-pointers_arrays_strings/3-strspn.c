21c2e29420537b8bee6ec1dca2930563ff840f73
Akhaze Jesse <jasonreacher47@gmail.com>
2022-09-27 13:46:00 -0700

    a

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strspn - Entry
+ * @s: string
+ * @accept: substring
+ * Return: consistC
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int consistC = 0;
+	int prevC;
+
+	while (*s)
+		{
+		i = 0;
+		prevC = consistC;
+		while (*(accept + i) != '\0')
+			{
+			if (*(accept + i) == *s)
+				{
+				consistC++;
+				}
+			i++
+			}
+		if (prevC == consistC)
+			{
+			break;
+			}
+		s++;
+		}
+
+	return (consistC);
+}
