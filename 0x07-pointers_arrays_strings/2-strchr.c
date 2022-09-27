a5291db0aa3e2efd2a58ecc5095ddb0a9b547a70
Akhaze Jesse <jasonreacher47@gmail.com>
2022-09-27 10:23:25 -0700

    a

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry
+ * @s: string to check
+ * @c: char to find
+ * Return: c, otherwise null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i))
+		{
+		if (*(s + i) == c)
+			return (s + i);
+			i++
+		}
+	if (*(s + i) == c)
+		return (s + i);
+
+	return (NULL);
+}
