c53bc6b12523f5316451abe9e77e81fd79275296
Akhaze Jesse <jasonreacher47@gmail.com>
2022-10-05 14:07:50 -0700

    a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * @argc: num of arg
+ * @argv: array of arg
+ * Return: a
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+	int a;
+
+	for (i = 0; i < argc; i++)
+		{
+		a = i;
+		}
+
+	printf("%d\n", a);
+
+	return (0);
+}
