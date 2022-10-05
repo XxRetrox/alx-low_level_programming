1117d885f916742808f5f5e601e732a7a1a25b1f
Akhaze Jesse <jasonreacher47@gmail.com>
2022-10-05 16:47:37 -0700

    a

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv)
 {
-	int cents, coins = 0;
+	int cents, coins;
 
 	if (argc != 2)
 		{
@@ -24,18 +24,22 @@ int main(int argc, char **argv)
 		if ((cents - 25) >= 0)
 			{
 			cents -= 25;
+			continue;
 			}
 		if ((cents - 10) >= 0)
 			{
 			cents -= 10;
+			continue;
 			}
 		if ((cents - 5) >= 0)
 			{
 			cents -= 5;
+			continue;
 			}
 		if ((cents - 2) >= 0)
 			{
 			cents -= 2;
+			continue;
 			}
 		cents--;
 		}
