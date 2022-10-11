cf124235ba14111f4bb5a981d97d030b1f2720b8
Akhaze Jesse <jasonreacher47@gmail.com>
2022-10-11 14:54:44 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+
+#define _DOG_H_
+
+/**
+ * struct dog - Entry
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Dog: pet dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
