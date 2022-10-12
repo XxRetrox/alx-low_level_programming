c539fa2a28bb1d8796839a5cedf5a5da7eabacd8
Akhaze Jesse <jasonreacher47@gmail.com>
2022-10-12 16:10:20 -0700

    a

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Entry
+ * @array: array
+ * @size: size of array
+ * @action: callback function
+ * Return: 0
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL || action == NULL)
+		{
+		return;
+		}
+
+	for (i = 0; i < size; i++)
+		{
+		(*action)(array[i]);
+		}
+}
